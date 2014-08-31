bc4a60448b764b78ccf33f297d2f45112a1b7f9d
vasarhelyia <vasarhelyi.agi@gmail.com>
2014-08-31 12:02:42 +0200

    Added PersistentStorage singleton class with test project for example usage.

---
+++ SwiftStorageTest/SwiftStorageTest/PersistentStorage.swift
@@ -0,0 +1,89 @@
+//
+//  PersistentStorage.swift
+//  SwiftStorageTest
+//
+//  Created by Ágnes Vásárhelyi on 31/08/14.
+//  Copyright (c) 2014 Ágnes Vásárhelyi. All rights reserved.
+//
+
+import Foundation
+
+private var Instance = PersistentStorage()
+private let StorageName = "MyAppStorage" //Replace with yours
+
+class PersistentStorage : NSObject {
+    
+    /* Always call self.save() in property's didSet:
+    didSet {
+    self.save()
+    } */
+    
+    var userName: String = "" {
+        didSet {
+            self.save()
+        }
+    }
+    
+    var numberOfAppLaunches: Int32 = 0 {
+        didSet {
+            if numberOfAppLaunches > 0 {
+                self.save()
+            }
+        }
+    }
+    
+    class var sharedInstance : PersistentStorage {
+        return Instance
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    override class func initialize() {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if let storageData = userDefaults.dataForKey(StorageName) {
+            let unarchiver = NSKeyedUnarchiver(forReadingWithData: storageData)
+            Instance = unarchiver.decodeObject() as PersistentStorage
+        } else {
+            Instance = PersistentStorage()
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        /* Call aDecoder.decodeObjectForKey(key:) for every property */
+        
+        if let userName = aDecoder.decodeObjectForKey("userName") as? String {
+            self.userName = userName
+        } else {
+            self.userName = ""
+        }
+        
+        self.numberOfAppLaunches = aDecoder.decodeIntForKey("numberOfAppLaunches")
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        /* Call aCoder.encodeObject(objv:, forKey:) for every property */
+        
+        aCoder.encodeObject(self.userName, forKey: "userName")
+        aCoder.encodeInt(self.numberOfAppLaunches, forKey: "numberOfAppLaunches")
+    }
+    
+    func save() {
+        let lock = NSLock()
+        lock.lock()
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let storageData = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: storageData);
+        
+        archiver.encodeObject(Instance)
+        archiver.finishEncoding()
+        
+        userDefaults.setObject(storageData, forKey: StorageName)
+        userDefaults.synchronize()
+        
+        lock.unlock()
+    }
+    
+}
---
+++ SwiftStorageTest/SwiftStorageTestTests/SwiftStorageTestTests.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftStorageTestTests.swift
+//  SwiftStorageTestTests
+//
+//  Created by Ágnes Vásárhelyi on 31/08/14.
+//  Copyright (c) 2014 Ágnes Vásárhelyi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftStorageTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        PersistentStorage()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSavingUserName() {
+        var testName = "My favorite user"
+        PersistentStorage.sharedInstance.userName = testName
+       
+        XCTAssertNotNil(PersistentStorage.sharedInstance.userName)
+        XCTAssertEqual(PersistentStorage.sharedInstance.userName, testName, "Saving userName property succeeded")
+    }
+    
+    func testSavingNumberOfAppLaunches() {
+        var originalNumberOfAppLaunches = PersistentStorage.sharedInstance.numberOfAppLaunches
+        PersistentStorage.sharedInstance.numberOfAppLaunches++
+        
+        XCTAssertEqual(PersistentStorage.sharedInstance.numberOfAppLaunches, originalNumberOfAppLaunches + 1, "Saving numberOfAppLaunches property succeeded")
+    }
+    
+}
